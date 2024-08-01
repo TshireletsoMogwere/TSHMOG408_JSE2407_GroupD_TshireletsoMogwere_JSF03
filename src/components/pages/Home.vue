4d721a8383374205a449f03c118ea2d0d8ef93bc
Tshireletso <tmogwere09@gmail.com>
2024-08-01 20:02:41 +0200

    creates home component

---
+++ src/components/pages/Home.vue
@@ -0,0 +1,29 @@
+<script>
+import { ref } from 'vue';
+import Header from '../Header.vue';
+import FetchData from '../FetchData.vue';
+
+export default {
+  components: {
+    Header,
+    FetchData,
+  },
+  setup() {
+    const selectedCategory = ref('All');
+
+    const setSelectedCategory = (category) => {
+      selectedCategory.value = category;
+    };
+
+    return { selectedCategory, setSelectedCategory };
+  },
+};
+</script>
+
+<template>
+    <div>
+      <Header @category-select="setSelectedCategory" />
+      <h1>Product List</h1>
+      <FetchData :selectedCategory="selectedCategory" />
+    </div>
+  </template>
